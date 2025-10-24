1d3fa98995c742994f2ef77b31eaf08734a574b8
gmulonga <gadsonmulonga@gmail.com>
2025-10-24 12:25:11 +0300

    feat: adding the empty state

---
+++ lib/widgets/empty_state.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+  final String message;
+  final IconData icon;
+
+  const EmptyState({
+    super.key,
+    required this.message,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 64, color: Colors.grey[400]),
+            const SizedBox(height: 16),
+            Text(
+              message,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                color: Colors.grey[600],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
